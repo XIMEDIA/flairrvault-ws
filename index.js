f1dbfd5d62945eff67ae61e1e7e171e859e91bdf
sopheapchea <sopheap@ximedialabs.com>
2020-09-01 10:27:47 -0400

    log clients

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ function unsubscribeAccounts(ws, accounts) {
 }
 
 function printStats() {
-  const connectedClients = wss.clients.length;
+  const connectedClients = wss.clients.size;
   const tps = tpsCount / statTime;
   console.log(wss.clients);
   console.log(`[Stats] Connected clients: ${connectedClients}; TPS Average: ${tps}`);
